ac92424625b79f320f53c6b493ed48a3c93168c8
gomenasaai <contramundee@gmail.com>
2014-06-08 18:21:28 +0100

    menu has prices when initialised

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,9 @@
 class TakeAway
 
+	def initialize
+		@total = 0
+		@price_list ||= []
+		@prices = {pizza: 5, burger: 3.20, pasta: 2.10}
+	end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,11 @@
 require 'takeaway'
 
 describe TakeAway do
+
+	let(:takeaway) {TakeAway.new}
+	
+	it 'has menu prices when initialised' do
+		expect(takeaway.prices).to eq({pizza: 5, burger: 3.20, pasta: 2.10})
+	end
 	
 end
