62812900508e04c44d4fae5f2b3f20c48819c475
gomenasaai <contramundee@gmail.com>
2014-06-08 18:22:18 +0100

    can print menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,4 +6,13 @@ class TakeAway
 		@prices = {pizza: 5, burger: 3.20, pasta: 2.10}
 	end
 
+	def print_menu
+		menu = "Welcome to Makers Take Away!\n"
+		menu += "This is the list of dishes you can order:\n"
+		menu += "pizza - 5.00 each\n"
+		menu += "burger - 3.20 each\n"
+		menu += "pasta - 2.10 each\n"
+		menu += "type pizza, burger or pasta\n"
+	end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,5 +7,15 @@ describe TakeAway do
 	it 'has menu prices when initialised' do
 		expect(takeaway.prices).to eq({pizza: 5, burger: 3.20, pasta: 2.10})
 	end
+
+	it 'can print menu' do
+		menu = "Welcome to Makers Take Away!\n"
+		menu += "This is the list of dishes you can order:\n"
+		menu += "pizza - 5.00 each\n"
+		menu += "burger - 3.20 each\n"
+		menu += "pasta - 2.10 each\n"
+		menu += "type pizza, burger or pasta\n"
+		expect(takeaway.print_menu).to eq menu
+	end
 	
 end
