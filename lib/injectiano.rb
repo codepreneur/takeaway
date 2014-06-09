f790d8cead2a572003b4f83cca2e4e4c8daf9249
gomenasaai <contramundee@gmail.com>
2014-06-09 13:11:05 +0100

    replaced delete_at(0) with shift

--- lib/injectiano.rb
+++ lib/injectiano.rb
@@ -1,19 +1,19 @@
 class Array
 
 	# block.call approach
-	def injectiano(accumulator=self.delete_at(0), &block)
+	def injectiano(accumulator=self.shift, &block)
 		self.each {|item| accumulator = block.call(accumulator,item)}
 		accumulator
 	end
 
 	# yield approach
-	def injectiano_yield(result=self.delete_at(0))
+	def injectiano_yield(result=self.shift)
 		self.each{|el| result = yield result, el}
 		result
 	end
 
 	# recursive approach
-	def injectiano_recursivo(result=self.delete_at(0),&block)
+	def injectiano_recursivo(result=self.shift,&block)
 		# need to implement it
 	end
 
