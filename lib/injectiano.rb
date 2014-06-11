33b2a10ce2441724afc5a39f192dd27747404ceb
gomenasaai <contramundee@gmail.com>
2014-06-11 13:54:47 +0100

    latest injectiano changes

--- lib/injectiano.rb
+++ lib/injectiano.rb
@@ -1,14 +1,20 @@
 class Array
 
 	# block.call approach
-	def injectiano(accumulator=0,&block)
-		self.each {|item| accumulator = block.call(accumulator,item)}
+	def injectiano(accumulator = nil,&block)
+		collection = self.dup
+
+		accumulator ||= collection.shift
+		collection.each {|item| accumulator = block.call(accumulator,item)}
 		accumulator
 	end
 
 	# yield approach
-	def injectiano_yield(result=0)
-		self.each{|el| result = yield result, el}
+	def injectiano_yield(result=self[0])
+		result == nil? collection = self.dup : self.drop(1)
+
+		collection ||= collection.shift
+		collection.each{|el| result = yield result, el}
 		result
 	end
 
